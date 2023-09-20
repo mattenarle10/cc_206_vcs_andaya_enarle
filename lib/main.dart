ae5b5d87338cd63e916ef433f880927c5f4c2012
mattenarle10 <enarlem10@gmail.com>
2023-09-20 08:16:16 +0800

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // Matthew Enarle: "Konting bato, Konting Semento - Monumento! "
-// Alecxander Jamille Andaya: "  "
+// Alecxander Jamille Andaya: "When life gives you lemons, 
+// squeeze it and make a lemonade "
 
 import 'package:flutter/material.dart';
